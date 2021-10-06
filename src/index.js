0ea5950644e9569fc3b33d5ccae4e42ed947e8ba
Eugeniy e-marketing <u1-259@office107.local>
2021-10-06 17:34:37 +0300

    MagicBall

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import AboutClub from './AboutClub';
 import symbol from './images/symbol.png'
 import symbolShakhtar from "./images/shakhtar.jpg"
-
+import MagicBall from "./magicBall"
 
 
 
@@ -11,11 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <AboutClub clubsName='Dynamo' country='Kiev' symbol={symbol} alt="dynamo-symbol"aboutClubClass="aboutClubDynamo" stars="2" wins="500" composition={['Бущан','Рамирес','Сирота','Миколенко']}/>
     <AboutClub clubsName='Shakhtar' country='Doneck' symbol={symbolShakhtar} alt="shakhtar-symbol" aboutClubClass="aboutClubShakhtar" stars="3" wins="600"composition={['Шевченко','Пузанов','Пятов','Трубин']}/>
+    <MagicBall/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
