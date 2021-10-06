db2ee3ed07825709bd89fbe41b86bd80f46a421a
Eugeniy e-marketing <u1-259@office107.local>
2021-10-06 12:43:33 +0300

    about-fc-homework

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import AboutClub from './AboutClub';
+import symbol from './images/symbol.png'
+import symbolShakhtar from "./images/shakhtar.jpg"
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AboutClub clubsName='Dynamo' country='Kiev' symbol={symbol} alt="dynamo-symbol"aboutClubClass="aboutClubDynamo" stars="2" wins="500" composition={['Бущан','Рамирес','Сирота','Миколенко']}/>
+    <AboutClub clubsName='Shakhtar' country='Doneck' symbol={symbolShakhtar} alt="shakhtar-symbol" aboutClubClass="aboutClubShakhtar" stars="3" wins="600"composition={['Шевченко','Пузанов','Пятов','Трубин']}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +18,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
